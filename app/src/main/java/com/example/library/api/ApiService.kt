ee2e9ac6afcbe6da1817a4a761a3af20f4a4767f
mehdipcd <mehdi.pcd@gmail.com>
2020-12-02 00:22:02 +0330

    Define Book model class and add a method to retrofit interface to get all books based on keyword

--- app/src/main/java/com/example/library/api/ApiService.kt
+++ app/src/main/java/com/example/library/api/ApiService.kt
@@ -1,4 +1,13 @@
 package com.example.library.api
 
+import com.example.library.Book
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface ApiService {
+
+    @GET("Book/GetAllByKeyword/")
+    suspend fun search(
+        @Path("keyword") keyword: String
+    ): List<Book>
 }
